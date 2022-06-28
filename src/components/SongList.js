0790ef782eeeea446009994e0776ba550e9b022d
Prateek Varma <prtk.varma@gmail.com>
2022-06-28 14:49:56 -0600

    Displayed the list of songs, by extracting Redux data from mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,14 +2,26 @@ import React from "react";
 import { connect } from "react-redux"; //using connect here, because this partiular component needs to pull in the data (i.e list of songs)
 
 class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return(
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      ) 
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log(state); //logs the 4 different song objects, and selectedSong as null
-  return state;
-}
+  return { songs: state.songs }; //this is equivalent to 'props.songs === { songs: state.songs }' being injected into the SongList component above. And this is how the data is passed from the REDUX store into a REACT component.
+};
 
 export default connect(mapStateToProps)(SongList); //yes this is valid syntax - calling a function inside a function.
