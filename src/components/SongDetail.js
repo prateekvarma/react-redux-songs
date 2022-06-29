fdc04abafb57887f55c30721075bc6ec8e3f3df7
Prateek Varma <prtk.varma@gmail.com>
2022-06-29 13:35:43 -0600

    final commit: Destructured props object into fields and displayed them on the component

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = (props) => {
-  console.log(props);
-  return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+    if(!song) {
+        //this is because the initial value of the selected song is 'null'
+        return <div>Please select a song!</div>;
+    }
+  return (
+    <div>
+        <h3>Song Details:</h3>
+        <p>Title: {song.title}</p>
+        <p>Duration: {song.duration}</p>
+    </div>
+  )
 };
 
 const mapStateToProps = (state) => {
